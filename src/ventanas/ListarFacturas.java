62ae69703ea19426f5edec9ee5c3fa58bcdf78bd
sebastianciocarlan <sebastianciocarlan@gmail.com>
2019-05-27 21:09:29 +0200

    ++Refactor && VisualUpdate

--- src/ventanas/ListarFacturas.java
+++ src/ventanas/ListarFacturas.java
@@ -11,39 +11,21 @@ import clases.Gestor;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
-public class ListarFacturas extends JDialog {
+public class ListarFacturasUsuario extends JDialog {
 
 
 	private static final long serialVersionUID = 5612853395117871603L;
 	private final JPanel contentPanel = new JPanel();
 	private JScrollPane scrollPane = new JScrollPane();
 	private final JTextPane textPane = new JTextPane();
-	private final JLabel lblDniCliente = new JLabel("Dni cliente:");
-	private final JTextField textDni = new JTextField();
-	private final JButton btnBuscar = new JButton("Buscar");
 
 
-	public ListarFacturas(Gestor gestor) {
-		textDni.setBounds(10, 36, 133, 20);
-		textDni.setColumns(10);
+	public ListarFacturasUsuario(Gestor gestor) {
 		inicializarGraficos();
-		handlers(gestor);
-		
+		setTextPaneText(gestor.listarFacturas());
 	}
-	public void handlers(Gestor gestor) {
-		btnBuscar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setTextPaneText(gestor.listarFacturasCliente(textDni.getText()));
-			}
-		});
 
-	}
 	public void inicializarGraficos() {
 		setResizable(false);
 		setBounds(100, 100, 608, 662);
@@ -52,21 +34,12 @@ public class ListarFacturas extends JDialog {
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 
-		scrollPane.setBounds(10, 82, 582, 540);
+		scrollPane.setBounds(10, 11, 582, 611);
 		contentPanel.add(scrollPane);
 		{
 			textPane.setEditable(false);
 			scrollPane.setViewportView(textPane);
 		}
-		lblDniCliente.setBounds(10, 11, 133, 14);
-		
-		contentPanel.add(lblDniCliente);
-		
-		contentPanel.add(textDni);
-
-		btnBuscar.setBounds(503, 35, 89, 23);
-		
-		contentPanel.add(btnBuscar);
 	}
 	public String getTextPaneText() {
 		return textPane.getText();
--- src/ventanas/ListarFacturasCliente.java
+++ src/ventanas/ListarFacturasCliente.java
@@ -11,21 +11,39 @@ import clases.Gestor;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class ListarFacturasUsuario extends JDialog {
+public class ListarFacturas extends JDialog {
 
 
 	private static final long serialVersionUID = 5612853395117871603L;
 	private final JPanel contentPanel = new JPanel();
 	private JScrollPane scrollPane = new JScrollPane();
 	private final JTextPane textPane = new JTextPane();
+	private final JLabel lblDniCliente = new JLabel("Dni cliente:");
+	private final JTextField textDni = new JTextField();
+	private final JButton btnBuscar = new JButton("Buscar");
 
 
-	public ListarFacturasUsuario(Gestor gestor) {
+	public ListarFacturas(Gestor gestor) {
+		textDni.setBounds(10, 36, 133, 20);
+		textDni.setColumns(10);
 		inicializarGraficos();
-		setTextPaneText(gestor.listarFacturas());
+		handlers(gestor);
+		
 	}
+	public void handlers(Gestor gestor) {
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setTextPaneText(gestor.listarFacturasCliente(textDni.getText()));
+			}
+		});
 
+	}
 	public void inicializarGraficos() {
 		setResizable(false);
 		setBounds(100, 100, 608, 662);
@@ -34,12 +52,21 @@ public class ListarFacturasUsuario extends JDialog {
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 
-		scrollPane.setBounds(10, 11, 582, 611);
+		scrollPane.setBounds(10, 82, 582, 540);
 		contentPanel.add(scrollPane);
 		{
 			textPane.setEditable(false);
 			scrollPane.setViewportView(textPane);
 		}
+		lblDniCliente.setBounds(10, 11, 133, 14);
+		
+		contentPanel.add(lblDniCliente);
+		
+		contentPanel.add(textDni);
+
+		btnBuscar.setBounds(503, 35, 89, 23);
+		
+		contentPanel.add(btnBuscar);
 	}
 	public String getTextPaneText() {
 		return textPane.getText();
